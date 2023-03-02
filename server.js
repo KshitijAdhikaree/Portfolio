701fd7113f86f2e525b176e86b552c664ed5bb06
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-03-02 18:56:03 +0545

    remove diploy to production from sever

--- server.js
+++ server.js
@@ -12,18 +12,18 @@ app.use("/api/portfolio", portfolioRoute);
 const port = process.env.PORT || 5000;
 const path = require("path");
 
-// Diploy to Production
-if (process.env.NODE_ENV === "production") {
-  const path = require("path");
-  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-  app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
-          if(err) {
-              res.status(500).send(err)
-          }
-      });
-  })
-}
+// // Diploy to Production
+// if (process.env.NODE_ENV === "production") {
+//   const path = require("path");
+//   app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+//   app.get("*", (req, res) => {
+//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+//           if(err) {
+//               res.status(500).send(err)
+//           }
+//       });
+//   })
+// }
 
 
 app.listen(port, () => {
