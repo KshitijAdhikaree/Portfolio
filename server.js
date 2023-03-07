f4a4400a6e463d9fde8fd067c83f5c09e94cba28
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-03-08 03:17:17 +0545

    update package

--- server.js
+++ server.js
@@ -12,13 +12,14 @@ app.use("/api/portfolio", portfolioRoute);
 const port = process.env.PORT || 5000;
 const path = require("path");
 
+
 // Diploy to Production
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
-  app.use(express.static(path.resolve(__dirname, "client", "build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.sendFile(
-      path.resolve(__dirname, "client", "build", "index.html"),
+      path.join(__dirname, "client/build/index.html"),
       function (err) {
         if (err) {
           res.status(500).send(err);
